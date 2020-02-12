5d65f2240dfeebfd1adc79c50a3aecb886d154d9
Logon PF Fiap <logonpflocal@fiap.com.br>
2020-02-11 22:59:08 -0300

    paramos aqui 11/02/2020

---
+++ js/app/controller/NegociacaoController.js
@@ -0,0 +1,17 @@
+class NegociacaoController {
+
+    
+
+    constructor() {
+        let $ = document.querySelector.bind(document)
+        this._inputData = $("#data")
+        this._inputQuantidade = $("#quantidade")
+        this._inputValor = $("#valor")
+
+    }
+
+    adiciona(event) {
+        $   
+        event.preventDefault()
+    }
+}
