3c395c5d7c9bd7271220240edf2a5f821e127b4c
Logon PF Fiap <logonpflocal@fiap.com.br>
2020-02-13 17:21:20 -0300

    mensagem e mensagem view

--- js/app/controller/NegociacaoController.js
+++ js/app/controller/NegociacaoController.js
@@ -8,6 +8,9 @@ class NegociacaoController {
         this._listaNegociacoes = new ListaNegociacoes()
         this._negociacoesView = new NegociacoesView($("#negociacoesView"))
         this._negociacoesView.update(this._listaNegociacoes)
+        this._mensagem = new Mensagem()
+        this._mensagemView = new MensagemView($("#mensagemView"))
+        this._mensagemView.update(this._mensagem)
     }
 
     adiciona(event) {
@@ -15,11 +18,9 @@ class NegociacaoController {
 
         this._listaNegociacoes.adiciona(this._criaNegociacao())
         this._negociacoesView.update(this._listaNegociacoes)
-        // this._listaNegociacoes.negociacoes.length = 0;
-        // this._listaNegociacoes.negociacoes.push(this._criaNegociacao())
+        this._mensagem.texto = "Negociação adicionada com sucesso"
+        this._mensagemView.update(this._mensagem)
         this._limpaFormulario()
-
-        // console.log("negociacao", this._listaNegociacoes.negociacoes)
     }
 
     _criaNegociacao() {
