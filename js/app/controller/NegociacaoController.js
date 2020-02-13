79fa960fb89fe04f98f8eaf77ab4f40376ce924b
Danilo Vitoriano <vitoriano08@gmail.com>
2020-02-13 19:23:06 -0300

    console

--- js/app/controller/NegociacaoController.js
+++ js/app/controller/NegociacaoController.js
@@ -52,11 +52,19 @@ class NegociacaoController {
         let xhr = new XMLHttpRequest();
         xhr.open('GET', 'http://localhost:3000/negociacoes/semana');
         xhr.onreadystatechange = () => {
-            if(xhr.readyState == 4) {
-                if(xhr.status == 200) {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                        .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao))
+                    this._mensagem.texto = 'Negociações importadas com sucesso.';
                     console.log('Obtendo as negociações do servidor.');
                     console.log(xhr.responseText);
+
                 } else {
+                    console.log(xhr.responseText);
+                    this._mensagem.texto = 'Não foi possível obter as negociações.';
+
                     console.log('Não foi possível obter as negociações do servidor.');
                     console.log(xhr.responseText);
                 }
