f10e79ffee2ebb1a96a7770c288cdc89b25fb1f6
Danilo Vitoriano <vitoriano08@gmail.com>
2020-02-13 10:33:35 -0300

    listaNegociacoes, DateHelper, criaNegociacao, limpaFormulario

--- js/app/controller/NegociacaoController.js
+++ js/app/controller/NegociacaoController.js
@@ -5,26 +5,32 @@ class NegociacaoController {
         this._inputData = $("#data")
         this._inputQuantidade = $("#quantidade")
         this._inputValor = $("#valor")
+        this._listaNegociacoes = new ListaNegociacoes()
     }
 
     adiciona(event) {
         event.preventDefault()
 
-        console.log(typeof (this._inputData.value));
-        console.log(this._inputData.value);
+        this._listaNegociacoes.adiciona(this._criaNegociacao())
+        // this._listaNegociacoes.negociacoes.length = 0;
+        // this._listaNegociacoes.negociacoes.push(this._criaNegociacao())
+        this._limpaFormulario()
 
-        let date = new Date(
-            ...this._inputData.value
-                .split('-')
-                .map((item, index) => item - index % 2)
-        )
+        console.log("negociacao", this._listaNegociacoes.negociacoes)
+    }
 
-        let negociacao = new Negociacao(
-            date,
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         )
+    }
 
-        console.log("negociacao", negociacao)
+    _limpaFormulario() {
+        this._inputData.value = ''
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.01;
+        this._inputData.focus();
     }
 }
