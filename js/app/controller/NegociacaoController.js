6eafefa27a02151d0bf2b675b3ddb2f14328c154
Danilo Vitoriano <vitoriano08@gmail.com>
2020-02-13 09:19:17 -0300

    data e transacao no console

--- js/app/controller/NegociacaoController.js
+++ js/app/controller/NegociacaoController.js
@@ -1,17 +1,30 @@
 class NegociacaoController {
 
-    
-
     constructor() {
         let $ = document.querySelector.bind(document)
         this._inputData = $("#data")
         this._inputQuantidade = $("#quantidade")
         this._inputValor = $("#valor")
-
     }
 
     adiciona(event) {
-        $   
         event.preventDefault()
+
+        console.log(typeof (this._inputData.value));
+        console.log(this._inputData.value);
+
+        let date = new Date(
+            ...this._inputData.value
+                .split('-')
+                .map((item, index) => item - index % 2)
+        )
+
+        let negociacao = new Negociacao(
+            date,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
+
+        console.log("negociacao", negociacao)
     }
 }
--- js/app/models/Negociacao.js
+++ js/app/models/Negociacao.js
@@ -1,25 +1,30 @@
 class Negociacao {
 
     constructor(data, quantidade, valor) {
-        this._data = new Date(data.getTime())
-        this._quantidade = quantidade
-        this._valor = valor
-        Object.freeze(this)
+        this._data = new Date(data);
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
+        console.log(this._data)
     }
 
-    get volume(){
-        return this._quantidade * this._valor
+    get volume() {
+
+        return this._quantidade * this._valor;
     }
 
     get data() {
-        return new Date(this._data.getTime())
+
+        return new Date(this._data);
     }
 
     get quantidade() {
-        return this._quantidade
+
+        return this._quantidade;
     }
-    
+
     get valor() {
-        return this._valor
+
+        return this._valor;
     }
 }
